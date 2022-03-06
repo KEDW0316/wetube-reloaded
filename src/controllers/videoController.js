63eb81dd4c09f8e8405f2bbd5b851912b294fb2a
unknown <dks01972@gmail.com>
2022-03-07 00:52:00 +0900

    5.8 iteration

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,13 @@
-export const trending = (req,res) => res.render("home");
-
-export const edit = (req,res) => res.send("Edit");
-export const search = (req,res) => res.send("Search");
-export const see = (req,res) => {
-    res.send(`Watch video#${req.params.id}`);
+const fakeUser = {
+    username:"Nicolas",
+    loggedIn: true,
 }
+
+export const trending = (req,res) => res.render("home", {pageTitle:"Home", fakeUser : fakeUser});
+
+export const edit = (req,res) => res.render("edit", {pageTitle : "Edit"});
+export const search = (req,res) => res.send("Search");
+export const see = (req,res) => res.render("watch", {pageTitle : "Watch Videos"});
 export const deleteVideo = (req,res) => res.send("delete Video");
 export const upload = (req,res) => res.send("Upload video");
 
