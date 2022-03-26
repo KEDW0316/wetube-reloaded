cf5d63c7e54d85927e5e67786718c2060262ff27
KEDW0316 <dks01972@gmail.com>
2022-03-26 23:52:44 +0900

    7.17 Github Login Part Two 5:55

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,48 +4,89 @@ import res, { redirect } from "express/lib/response";
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
-    try {
-        const videos = await Video.find({});
-        res.render("home", { pageTitle: "Home", videos });
-    } catch (error) {
-        return res.send("Server Error");
-    }
-}
+  try {
+    const videos = await Video.find({}).sort({ createdAt: "desc" });
+    res.render("home", { pageTitle: "Home", videos });
+  } catch (error) {
+    return res.send("Server Error");
+  }
+};
 
-export const getEdit = (req, res) => {
-    const id = req.params.id;
-    res.render("edit", { pageTitle: `Editing : ${video.title}` });
-}
+export const getEdit = async (req, res) => {
+  const id = req.params.id;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.status(404).render("404", { pageTitle: "Video Not Found" });
+  }
 
-export const watch = (req, res) => {
-    const id = req.params.id;
-    res.render("watch", { pageTitle: `Watching ${video.title}` });
-}
+  return res.render("edit", { pageTitle: `Edit ${video.title}`, video });
+};
 
-export const postEdit = (req, res) => {
-    const id = req.params.id;
-    const { title } = req.body;
-    return res.redirect(`/videos/${id}`);
-}
+export const postEdit = async (req, res) => {
+  const id = req.params.id;
+  const { title, description, hashtags } = req.body;
+  const video = await Video.exists({ _id: id });
+  if (!video) {
+    return res.status(404).redirect("404", { pageTitle: "Video Not Found" });
+  }
+  await Video.findByIdAndUpdate(id, {
+    title,
+    description,
+    hashtags: Video.formatHashtags(hashtags),
+  });
+
+  return res.redirect(`/videos/${id}`);
+};
+
+export const watch = async (req, res) => {
+  const id = req.params.id;
+  const video = await Video.findById(id);
+  if (!video) {
+    console.log("Video Not Found");
+    return res.status(404).render("404", { pageTitle: "Video Not Found" });
+  }
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+};
 
 export const getUpload = (req, res) => {
-    return res.render("upload", { pageTitle: "Upload Video" });
-}
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
 
-export const postUpload = (req, res) => {
-    //here we will add a video to the videos array.
-    const { title , description, hashtags} = req.body;
-    const video = new Video({
-        title,
-        description,
-        createdAt:Date.now(),
-        hashtags : hashtags.split(",").map(word => `#${word}`),
-        meta:{
-            views:0,
-            rating:0.
-        },
-    })
-    console.log(video);
-    
+export const postUpload = async (req, res) => {
+  //here we will add a video to the videos array.
+  const { title, description, hashtags } = req.body;
+  try {
+    await Video.create({
+      title,
+      description,
+      hashtags: Video.formatHashtags(hashtags),
+    });
     return res.redirect("/");
-}
+  } catch (error) {
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
+};
+
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
+  return res.redirect("/");
+};
+
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(keyword, "i"),
+      },
+    });
+    return res.render("search", { pageTitle: "Search", videos });
+  }
+
+  return res.render("search", { pageTitle: "Search", videos });
+};
--- src/init.js
+++ src/init.js
@@ -1,8 +1,10 @@
+import "dotenv/config";
 import "./db";
 import "./models/Video";
+import "./models/User";
 import app from "./server";
 const PORT = 4000;
 
-const handleListening = () => 
-    console.log(`server listening on port http://localhost:${PORT}`);
+const handleListening = () =>
+  console.log(`server listening on port http://localhost:${PORT}`);
 app.listen(PORT, handleListening);
